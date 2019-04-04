18c2c64944b2e519b41287be3e5310d17dac312e
asarkar5 <ashoke.k.sarkar@gmail.com>
2019-04-03 21:28:21 -0400

    Initial commit

---
+++ src/main/java/com/springboot_12aks/springboot12aks/ActorRepository.java
@@ -0,0 +1,6 @@
+package com.springboot_12aks.springboot12aks;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long>{
+}
---
+++ src/test/java/com/springboot_12aks/springboot12aks/Springboot12aksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot_12aks.springboot12aks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot12aksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
